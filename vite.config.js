79f2139a12e5b86852c528bb5981a5cebf978218
Purushottam Singram <87688500+inxeoz@users.noreply.github.com>
2025-03-13 00:54:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Change 'dist' to your desired output folder name
+  },
+});
