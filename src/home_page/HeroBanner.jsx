3722427bc943b077bbb7a39235f08cfc80011e53
inxeoz <inxeoz@mail.com>
2025-03-14 01:16:33 +0530

    working on slider

--- src/home_page/HeroBanner.jsx
+++ src/home_page/HeroBanner.jsx
@@ -1,15 +1,48 @@
-import React from 'react';
-import './HeroBanner.css'; // Create this CSS file for styling
+import React, { useState } from 'react';
+import './HeroBanner.css';
+import image1 from './isro.png';
+import image2 from './slide.jpg';
 
 const HeroBanner = () => {
+    const images = [image1, image2];
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const [imageClasses, setImageClasses] = useState(['hero-image']);
+
+    const handleDotClick = (index) => {
+        setImageClasses(['hero-image', 'hidden']);
+        setTimeout(() => {
+            setCurrentImageIndex(index);
+            setImageClasses(['hero-image']);
+        }, 500);
+    };
+
+    const handlePrev = () => {
+        const newIndex = (currentImageIndex - 1 + images.length) % images.length;
+        handleDotClick(newIndex);
+    };
+
+    const handleNext = () => {
+        const newIndex = (currentImageIndex + 1) % images.length;
+        handleDotClick(newIndex);
+    };
+
     return (
         <div className="hero-banner">
-            <div className="hero-image">
-                {/* You can add your image here using an <img> tag or as a background image in CSS */}
+            <div className="arrow left-arrow" onClick={handlePrev}>
+                &lt;
+            </div>
+            <div className={imageClasses.join(' ')} style={{ backgroundImage: `url(${images[currentImageIndex]})` }}></div>
+            <div className="arrow right-arrow" onClick={handleNext}>
+                &gt;
             </div>
             <div className="slider-dots">
-                <span className="dot active"></span>
-                <span className="dot"></span>
+                {images.map((_, index) => (
+                    <span
+                        key={index}
+                        className={`dot ${currentImageIndex === index ? 'active' : ''}`}
+                        onClick={() => handleDotClick(index)}
+                    ></span>
+                ))}
             </div>
         </div>
     );
